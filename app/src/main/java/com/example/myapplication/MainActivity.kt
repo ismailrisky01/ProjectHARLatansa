9b61fedfd3b94696dd42fc8f86ce642b4581a874
ismailrisky01 <ismailrisky01@gmail.com>
2023-08-11 18:23:45 +0700

    init Tesis

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,20 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.ml.*
 import org.tensorflow.lite.DataType
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.model.Model
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity(),SensorEventListener {
     var state: State = State.STAND
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     var acc = Vector(0f, 0f, 0f)
     var gyro = Vector(0f, 0f, 0f)
     val stack = Stack()
+    val mode = Modus()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,9 +39,11 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         onStartRecord()
     }
+    // onStartRecord merupakan fungsi untuk memulai sensor
     fun onStartRecord() {
         machineLearning = MachineLearning(baseContext)
 
+        //untuk meregristasi sensor accelero dan gyro
         sensorManager.registerListener(
             this,
             sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
@@ -50,11 +57,13 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
             SensorManager.SENSOR_DELAY_UI
         )
     }
-    private fun onStopRecord() {
+//    private fun onStopRecord() {
+//
+//        sensorManager.unregisterListener(this)
+//
+//    }
 
-        sensorManager.unregisterListener(this)
-
-    }
+    //berfungsi sebagai listener nilai sensor yang keluar, lalu setelah mendapat nilai, nilai akan di simpan ke dalam list dan dilakukan prediksi oleh varibale result
     override fun onSensorChanged(event: SensorEvent?) {
         if (event == null) return
 
@@ -68,7 +77,20 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         dataSets.add(dataset)
         stack.insert(dataset)
         val result = machineLearning.predict(stack.getArray())
-     Log.d("Latansa",result.toString())
+        Log.d("Latansa",result.toString())
+        val text = findViewById<TextView>(R.id.result)
+        val textRealtime = findViewById<TextView>(R.id.resultRealtime)
+        mode.insert(result.toString())
+        textRealtime.text = result.toString()
+
+        if (mode.getModus()!=""){
+            text.text = mode.getModus()
+            mode.clear()
+        }else{
+
+        }
+
+//        Log.d("Latansa","Bismillah")
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -76,16 +98,21 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
 
 
 }
+
+//Pembuatan model Vector
 data class Vector(val x: Float, val y: Float, val z: Float)
 
+//Pembuatan model Dataset
 data class DataSet(var accelerometer: Vector, var gyroscope: Vector, val state: State)
 
-//enum class State(val category: Int) { UNKNOWN(0), SIT(1), STAND(2), WALK(3), SLEEP(4) }
+//Model State, berisi fitur"
 enum class State(val category: Int) { SIT(0), STAND(1), WALK(2), SLEEP(3), UPSTAIRS(4), DOWNSTAIRS(5), STAND2SIT(6), STAND2SLEEP(7), SIT2STAND(8), SIT2SLEEP(9), SLEEP2STAND(10), SLEEP2SIT(11), UNKNOWN(12), JUMP(13), TORIGHT(14), TOLEFT(15) }
+//enum class State(val category: Int) { UNKNOWN(0), SIT(1), STAND(2), WALK(3), SLEEP(4) }
 
+//Class untuk membuat tumpukan yang nanti akan dimasukan ke prediksi
 class Stack {
     private val arraylist = ArrayList<Array<Float>>().apply {
-        for (i in 0 until 128) add(arrayOf(0f, 0f, 0f, 0f, 0f, 0f, 1f))
+        for (i in 0 until 24) add(arrayOf(0f, 0f, 0f, 0f, 0f, 0f, 1f))
     }
 
     private fun convert(item:DataSet): Array<Float> {
@@ -97,7 +124,7 @@ class Stack {
     }
 
     fun insert(item: DataSet) {
-        if (arraylist.count() >= 128)
+        if (arraylist.count() >= 24)
             arraylist.removeFirst()
         arraylist.add(convert(item))
     }
@@ -106,8 +133,61 @@ class Stack {
         return arraylist.toTypedArray()
     }
 }
+class Modus{
+    private val arrayList = ArrayList<String>()
+    fun insert(string: String){
+        arrayList.add(string)
+    }
+    fun getModus():String{
+        if (arrayList.size==15){
+           return modus()
+        }else{
+            return ""
+        }
+    }
+    fun clear(){
+        arrayList.clear()
+    }
+    fun modus(): String {
+
+        var previous = arrayList[0]
+        var modus = arrayList[0]
+        var count = 1
+        var maxCount = 1
+
+        for (i in 1 until arrayList.size) {
+            if (arrayList[i] == previous) {
+                count++
+            }
+            else {
+                if (count > maxCount) {
+                    modus = arrayList[i - 1]
+                    maxCount = count
+                }
+                previous = arrayList[i]
+                count = 1
+            }
+        }
+
+        if (count > maxCount) {
+            return arrayList[arrayList.size - 1]
+        } else {
+            return modus
+        }
+    }
 
 
+}
+
+fun main() {
+    val list= listOf("a","b","a","a","b","b","b","b","b","b","b")
+    val modus = Modus()
+    list.forEach {
+        modus.insert(it)
+    }
+    println("Value: "+modus.modus())
+}
+//Merubah data array ke data buffer
 fun createBuffer(dataset: Array<Array<Float>>): ByteBuffer {
     val buffer = ByteBuffer.allocateDirect(dataset.size * dataset[0].size * 4)
     for (data in dataset) for (value in data) buffer.putFloat(Random().nextFloat())
@@ -115,15 +195,22 @@ fun createBuffer(dataset: Array<Array<Float>>): ByteBuffer {
 }
 
 
+//Fungsi untuk melakukan pemanggilan Model dan prediksi
 class MachineLearning(context: Context) {
-    private val model = ConvertedModelBaru.newInstance(context)
-    private val inputLayer = TensorBuffer.createFixedSize(intArrayOf(128, 7), DataType.FLOAT32)
+    val compatList = CompatibilityList()
+
+    val options = if(compatList.isDelegateSupportedOnThisDevice) {
+        // if the device has a supported GPU, add the GPU delegate
+        Model.Options.Builder().setDevice(Model.Device.GPU).build()
+    } else {
+        // if the GPU is not supported, run on 4 threads
+        Model.Options.Builder().setNumThreads(4).build()
+    }
+    private val model = NewModel4.newInstance(context)
+    private val inputLayer = TensorBuffer.createFixedSize(intArrayOf(24, 7), DataType.FLOAT32)
 
     fun predict(dataset: Array<Array<Float>>): State? {
-        Log.d("dataset", dataset.size.toString())
-        Log.d("dataset", dataset[0].size.toString())
-        inputLayer.loadBuffer(createBuffer(dataset))
-
+        inputLayer.loadBuffer(createBuffer(dataset), intArrayOf(24, 7))
         val output = model.process(inputLayer).outputFeature0AsTensorBuffer
         val list = arrayOf(
             output.getFloatValue(0),
@@ -141,7 +228,7 @@ class MachineLearning(context: Context) {
             output.getFloatValue(12),
             output.getFloatValue(13),
             output.getFloatValue(14),
-            output.getFloatValue(15)
+            output.getFloatValue(15),
 
         )
         val value = list.indices.maxByOrNull { list[it] } ?: 0
